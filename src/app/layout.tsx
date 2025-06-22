08a58ba14cb141b4341417dbe6b636a4724c4f83
trannguyenvu3482 <trannguyenvu3482@gmail.com>
2025-06-22 18:19:06 +0700

    fix: remove test code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <div>test</div>
       </body>
     </html>
   );
